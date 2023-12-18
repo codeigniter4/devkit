9dd841d148f97a8f4954c5ed81edab582f152497
kenjis <kenji.uui@gmail.com>
2023-12-18 19:56:42 +0900

    docs: break long comment

--- src/Template/rector.php
+++ src/Template/rector.php
@@ -113,7 +113,9 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig
         ->ruleWithConfiguration(TypedPropertyFromAssignsRector::class, [
             /**
-             * The INLINE_PUBLIC value is default to false to avoid BC break, if you use for libraries and want to preserve BC break, you don't need to configure it, as it included in LevelSetList::UP_TO_PHP_74
+             * The INLINE_PUBLIC value is default to false to avoid BC break,
+             * if you use for libraries and want to preserve BC break, you don't
+             * need to configure it, as it included in LevelSetList::UP_TO_PHP_74
              * Set to true for projects that allow BC break
              */
             TypedPropertyFromAssignsRector::INLINE_PUBLIC => true,
